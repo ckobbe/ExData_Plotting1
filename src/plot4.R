3405a962197af308d5afcc3ae1dcfacd96b48478
ckobbe <c.kobbe@gmail.com>
2014-09-05 01:54:09 -0500

    Fixed panel plot
    removed the border from the replot of plot 3

--- src/plot4.R
+++ src/plot4.R
@@ -28,7 +28,7 @@ plot4 <- function() {
     plot(dateTime,Sub_metering_1, xlab="", ylab="Energy Sub Metering",type="l")
     points(dateTime,Sub_metering_2,col="red",type="l")
     points(dateTime,Sub_metering_3,col="blue",type="l")
-    legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+    legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="n")
     # Voltage by datetime
     plot(dateTime,Voltage, xlab="datetime", ylab="Voltage",type="l")
     # Global_reative_power by datetime
